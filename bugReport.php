e9bfab3b12c02242ec1722c2ff4a610980717c24
niveditanadkarni <nivedita.nadkarni@sjsu.edu>
2017-05-16 22:06:53 -0700

    sidebar

--- bugReport.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>...</head>
-<body>
-<h1>Hello World!</h1>
-<script type="text/javascript" src="https://crowstestmanager.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-o59b17/b/c/7ebd7d8b8f8cafb14c7b0966803e5701/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=d7ac7581"></script>
-
-
-</body>
-</html>
